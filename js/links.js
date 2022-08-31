3f7ab902b1b9f09e8f29ad3a10ab5f1c13eb585a
wenned <wenned@gmail.com>
2022-08-31 14:11:03 -0300

    Atualização JavaCript / Correção Html e CSS

--- js/links.js
+++ js/links.js
@@ -2,7 +2,7 @@ function linK(){
     var lin = document.getElementById('link')
     lin.innerHTML = `
     <li><a href="download/index.html" title="HyperText Markup Language" style="color: #232399" download="txt/html">Download HTML Index</a></li>
-    <li><a href="download/projeto1.html" title="HyperText Markup Language" style="color: #232399 download="txt/html">Download HTML Pag.Projeto</a></li>
+    <li><a target="_blank" href="download/projeto.txt" title="HyperText Markup Language" style="color: #232399 download="txt/html">Download HTML Pag.Projeto</a></li>
     <li><a href="download/style.css" title="Cascading Style Sheets" style="color: #232399 download="txt/css">Download CSS</a></li>
     <li><a href="download/links.js" title="JavaScript" style="color: #232399 download="application/ecmascript">Download código JS</a></li> 
     `
@@ -27,11 +27,11 @@ function linK1(){
     var res = document.querySelector('ol#link1')
     
     res.innerHTML = `
-        <li><a href="download/img/cinza.png" style="color: #232399">Pagina Inteira</a></li>
-        <li><a href="download/img/cinza.png" style="color: #232399">Area Projeto</a></li>
-        <li><a href="download/img/cinza.png" style="color: #232399">Area Sobre</a></li>
-        <li><a href="download/img/cinza.png" style="color: #232399">Area Contato</a></li>
-        <li><a href="download/img/cinza.png" style="color: #232399">Estrutura Projeto</a></li>
+        <li><a target="_blank" href="download/img/paginainteira.png" style="color: #232399">Pagina Inteira</a></li>
+        <li><a target="_blank" href="download/img/areaprojeto.png" style="color: #232399">Area Projeto</a></li>
+        <li><a target="_blank" href="download/img/sobre.png" style="color: #232399">Area Sobre</a></li>
+        <li><a target="_blank" href="download/img/areacontato.png" style="color: #232399">Area Contato</a></li>
+        <li><a target="_blank" href="download/img/estrutura.png" style="color: #232399">Estrutura Projeto</a></li>
     
     `
 

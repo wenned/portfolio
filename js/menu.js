fd5320cc98f92f6d831831b33ec87c54a4950665
wenned <wenned@gmail.com>
2022-11-05 14:31:08 -0300

    atualizacao menumob index

--- js/menu.js
+++ js/menu.js
@@ -37,3 +37,14 @@ function hiddenMenu() {
 
 
 }
+
+let ativa = document.querySelector('.toggle')
+let ativamenu = document.querySelector('.menuPrincipal')
+let men = document.querySelector('.menu')
+
+ativa.onclick = function(){
+    ativamenu.classList.toggle('active')
+    ativa.classList.toggle('active')
+    men.classList.toggle('active')
+
+}

e328944606d209f81ef0fe9cf7476ab76a496dad
wenned <wenned@gmail.com>
2023-01-21 13:39:48 -0300

    Atualizacao rotas v6

--- portfoli/src/App.js
+++ portfoli/src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+import Projetos from './pages/Projetos'
+import Contato from './pages/Contato'
+
+
+//import './App.css';
 
 function App() {
   return (
     <div className="App">
+
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route path="/Sobre" element={<Sobre/>}/>
+          <Route path="/Projetos" element={<Projetos/>}/>
+          <Route path="/Contato" element={<Contato/>}/>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
